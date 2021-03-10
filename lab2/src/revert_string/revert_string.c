ce8bc8ac56c08b199a9aa9190129e9e5b29099ef
repl.it user <replituser@example.com>
2021-03-10 23:25:59 +0000

    1-2 done

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,15 @@
 #include "revert_string.h"
+#include "string.h"
 
 void RevertString(char *str)
 {
-	// your code here
+	int left,right;
+    char buf;
+    for(left=0, right=strlen(str)-1; left<right; left++, right--)
+    {
+        buf = *(str+left);
+        *(str+left) = *(str+right);
+        *(str+right) = buf;
+    }
 }
 
